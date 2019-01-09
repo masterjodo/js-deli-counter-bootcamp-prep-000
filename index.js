dfe7bf5120d961c397e552996f7984b6ab985f0d
Jawad Ashour <jawadashour@gmail.com>
2019-01-09 02:06:35 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -19,6 +19,6 @@ function currentLine(deliLine) {
   if (deliLine.length === 0) {
     return("The line is currently empty.")
   } else{
-    return("The line is currently: " + aline);
+    return("The line is currently:" + aline);
   }
 }
